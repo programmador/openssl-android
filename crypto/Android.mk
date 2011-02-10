79a9512ffea97eeb6333f93378d288d00e0625fd
Hans-Christoph Steiner <hans@eds.org>
2011-02-10 15:04:20 -0500

    removed LOCAL_SHARED_LIBRARIES since that refers to the internal build

--- crypto/Android.mk
+++ crypto/Android.mk
@@ -523,32 +523,11 @@ ifeq ($(TARGET_ARCH),arm)
 else
 	LOCAL_SRC_FILES += $(non_arm_src_files)
 endif
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE:= libcrypto_static
-include $(BUILD_STATIC_LIBRARY)
-
-#######################################
-# target shared library
-include $(CLEAR_VARS)
-include $(LOCAL_PATH)/../android-config.mk
-
-ifneq ($(TARGET_ARCH),x86)
-LOCAL_NDK_VERSION := 5
-LOCAL_SDK_VERSION := 9
-# Use the NDK prebuilt libz and libdl.
-LOCAL_LDFLAGS += -lz -ldl
-else
-LOCAL_SHARED_LIBRARIES += libz libdl
-endif
-
-LOCAL_SRC_FILES += $(local_src_files)
-LOCAL_CFLAGS += $(local_c_flags)
-LOCAL_C_INCLUDES += $(local_c_includes)
-ifeq ($(TARGET_ARCH),arm)
-	LOCAL_SRC_FILES += $(arm_src_files)
-	LOCAL_CFLAGS += $(arm_cflags)
-else
-	LOCAL_SRC_FILES += $(non_arm_src_files)
+ifeq ($(TARGET_SIMULATOR),true)
+	# Make valgrind happy.
+	LOCAL_CFLAGS += -DPURIFY
+    LOCAL_LDLIBS += -ldl
+>>>>>>> removed LOCAL_SHARED_LIBRARIES since that refers to the internal build
 endif
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE:= libcrypto
